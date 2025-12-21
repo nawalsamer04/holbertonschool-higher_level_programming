4b7ec590c1d9b614d665dbc5a042979a934f2ca5
root <root@ip-10-42-111-103.ec2.internal>
2025-12-21 01:26:21 +0000

    Task 12: Pascal's Triangle

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,20 +1,33 @@
 #!/usr/bin/python3
-"""Module that defines pascal_triangle function."""
+"""
+Module that defines a function to generate Pascal's Triangle.
+"""
 
 
 def pascal_triangle(n):
-    """Return a list of lists of integers representing Pascal’s triangle of n."""
+    """
+    Returns a list of lists of integers representing
+    Pascal’s triangle of size n.
+
+    Args:
+        n (int): Number of rows of Pascal's triangle
+
+    Returns:
+        list: A list of lists of integers
+    """
     if n <= 0:
         return []
 
     triangle = []
 
     for i in range(n):
-        row = [1] * (i + 1)
+        row = [1]
 
-        if i > 1:
-            for j in range(1, i):
-                row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+        if triangle:
+            last_row = triangle[-1]
+            for j in range(1, len(last_row)):
+                row.append(last_row[j - 1] + last_row[j])
+            row.append(1)
 
         triangle.append(row)
 
