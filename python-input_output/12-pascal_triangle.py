facdc856e9bbaba3794b9ddaca42d63b894ce324
root <root@ip-10-42-111-103.ec2.internal>
2025-12-21 01:23:17 +0000

    Add Pascal's Triangle

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Module that defines pascal_triangle function."""
+
+
+def pascal_triangle(n):
+    """Return a list of lists of integers representing Pascal’s triangle of n."""
+    if n <= 0:
+        return []
+
+    triangle = []
+
+    for i in range(n):
+        row = [1] * (i + 1)
+
+        if i > 1:
+            for j in range(1, i):
+                row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+
+        triangle.append(row)
+
+    return triangle
