6aba0e3a7a6fbac244dea47903ed36dc973182e5
root <root@ip-10-42-111-103.ec2.internal>
2025-12-21 01:01:44 +0000

    Task 6: Create object from a JSON file

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+"""Module that defines load_from_json_file()."""
+
 import json
 
 
 def load_from_json_file(filename):
-    """Creates an object from a JSON file"""
+    """Create a Python object from a JSON file."""
     with open(filename, "r", encoding="utf-8") as f:
         return json.load(f)
