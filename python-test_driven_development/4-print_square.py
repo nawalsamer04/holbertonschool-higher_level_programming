8b5eb6d000a82d3d5a5902e245feb3fb6c330a64
nawalsamer04 <nawalsamer04@gmail.com>
2026-01-05 22:37:51 +0000

    Fix print_square missing argument error

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,23 +1,19 @@
 #!/usr/bin/python3
-"""Module that provides print_square function."""
+"""Module that prints a square using #."""
 
 
 def print_square(size):
     """Print a square with the character #.
 
     Args:
-        size (int): Size of the square.
-
-    Raises:
-        TypeError: If size is not an integer.
-        ValueError: If size is < 0.
+        size (int): size of the square
     """
-    if isinstance(size, float) and size < 0:
-        raise TypeError("size must be an integer")
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
+
     if size < 0:
         raise ValueError("size must be >= 0")
 
     for _ in range(size):
         print("#" * size)
+
