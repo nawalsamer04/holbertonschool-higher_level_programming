09529b303d1ba7c3bf888b5b7b90d9ed869522f3
nawalsamer04 <nawalsamer04@gmail.com>
2026-01-05 22:46:29 +0000

    Fix print_square: missing argument and validation

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -10,10 +10,8 @@ def print_square(size):
     """
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
-
     if size < 0:
         raise ValueError("size must be >= 0")
 
     for _ in range(size):
         print("#" * size)
-
