5941e9b9289f58055be98120f688bf167dac1dd5
nawalsamer04 <nawalsamer04@gmail.com>
2026-01-05 23:56:57 +0000

    Fix lazy matrix multiplication doctest

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-"""
-Module that multiplies two matrices using NumPy
-"""
+"""101-lazy_matrix_mul module"""
 import numpy as np
 
 
 def lazy_matrix_mul(m_a, m_b):
-    """
-    Multiply two matrices using NumPy
-    """
+    """Multiply two matrices using NumPy"""
     try:
-        return (np.matmul(m_a, m_b)).tolist()
-    except Exception as e:
-        raise e
+        return np.matmul(m_a, m_b)
+    except ValueError:
+        raise ValueError("m_a and m_b can't be multiplied")
+    except TypeError:
+        raise TypeError("Scalar operands are not allowed, use '*' instead")
+
