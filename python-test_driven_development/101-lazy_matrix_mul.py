cd67f8f90c5a260ac10ef8954e6ab45966dbb145
nawalsamer04 <nawalsamer04@gmail.com>
2026-01-06 00:00:28 +0000

    Add lazy matrix multiplication using NumPy

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
-"""101-lazy_matrix_mul module"""
+"""
+Module that multiplies two matrices using NumPy
+"""
 import numpy as np
 
 
 def lazy_matrix_mul(m_a, m_b):
-    """Multiply two matrices using NumPy"""
+    """
+    Multiply two matrices using NumPy
+    """
     try:
-        return np.matmul(m_a, m_b)
-    except ValueError:
-        raise ValueError("m_a and m_b can't be multiplied")
-    except TypeError:
-        raise TypeError("Scalar operands are not allowed, use '*' instead")
+        return (np.matmul(m_a, m_b)).tolist()
+    except Exception as e:
+        raise e
 
