eebe1b26a04f56ff6bb88c25e8cdede36f5f5e2a
nawalsamer04 <nawalsamer04@gmail.com>
2026-01-07 01:32:14 +0000

    Fix PEP8 line length in lookup

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""Defines a function that returns the list of available attributes and methods."""
+"""
+Defines a function that returns the list of available
+attributes and methods of an object.
+"""
 
 
 def lookup(obj):
