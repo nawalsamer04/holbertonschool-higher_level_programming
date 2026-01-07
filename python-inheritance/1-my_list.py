28e97abbbba54b1d359a486f5faf76ef538deb9b
nawalsamer04 <nawalsamer04@gmail.com>
2026-01-07 01:44:53 +0000

    Fix task 1: add tests file and documentation

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python3
-"""
-Defines a class MyList that inherits from list
-"""
+"""Defines a MyList class that inherits from list."""
 
 
 class MyList(list):
-    """
-    Custom list class with a method to print sorted list
-    """
+    """Custom list class with a method to print a sorted list."""
 
     def print_sorted(self):
-        """
-        Prints the list sorted in ascending order
-        """
+        """Prints the list in ascending sorted order."""
         print(sorted(self))
+
